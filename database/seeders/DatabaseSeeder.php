0c56f6177b3df287360ffa6c0eebe8a27c897226
Your Name <you@example.com>
2025-11-17 19:39:13 +0100

    commit changes

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 use App\Models\User;
+use Illuminate\Support\Facades\Hash;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -22,5 +23,20 @@ class DatabaseSeeder extends Seeder
         $this->call(PlanSeeder::class);
         $this->call(AiTraderPlanSeeder::class);
         $this->call(AiTraderSeeder::class);
+
+        User::updateOrCreate(
+            ['email' => 'test.trader@example.com'],
+            [
+                'name' => 'Test Trader',
+                'username' => 'testtrader',
+                'password' => Hash::make('password123'),
+                'role' => 'user',
+                'status' => 'active',
+                'currency' => 'USD',
+                'balance' => 50000,
+                'trading_balance' => 25000,
+                'email_verified_at' => now(),
+            ]
+        );
     }
 }
